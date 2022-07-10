cdc67f9692144fb805ade01c68677eb30187452c
shirliwisse <shirlimlm@gmail.com>
2022-07-10 21:49:15 +0300

    second  commit

--- users_management/views.py
+++ users_management/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User_Role, Ticket, Administrator
 
 # Create your views here.
+
+
+querysetUser_Role = User_Role.objects.all()
+querysetTicket = Ticket.objects.all()
+querysetAdministrator = Administrator.objects.all()
+
 
 projectsList = [
     {
