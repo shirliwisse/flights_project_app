df92035c6f3f1fccd1a486db1c3b97952cb976b0
inbagg <inbarginat@gmail.com>
2022-07-12 22:43:45 +0300

    serializer
    serialized roles for some reson

--- users_management/urls.py
+++ users_management/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('users_management/', views.users_management),
-    path('api', views.apiOverview, name='api-overview'),
+    path('api/', views.apiOverview, name='api-overview'),
+    path('roles-list/', views.rolesList, name='roles-list'),
+    path('roles-details/<str:pk>/', views.rolesDetail, name='roles-details'),
+    path('roles-create/', views.rolesCreate, name='roles-create'),
+    path('roles-update/<str:pk>/', views.rolesUpdate, name='roles-update'),
+    path('roles-delete/<str:pk>/', views.rolesDelete, name='roles-delete'),
 ]
